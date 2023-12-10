5244d4f6034d821bb8e56c040160630d89b4a30f
seiei-n <solehamugoiyo@gmail.com>
2023-12-10 18:17:28 +0900

    feat: 投稿用のスキーマ、SQL分設定

--- backend/main.go
+++ backend/main.go
@@ -12,6 +12,34 @@ import (
 
 const Template = "../frontend"
 
+const (
+	dbfileName = "../db/db.sqlite3"
+	// ブログポストテーブルを作成するSQL文
+	createPostTable = `CREATE TABLE IF NOT EXISTS posts (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT,
+		body TEXT,
+		author TEXT,
+		created_at TEXT
+	)`
+
+	// ブログポストテーブルにデータを挿入するSQL文
+	insertPostTable = `INSERT INTO posts (title, body, author, created_at) VALUES (?, ?, ?, ?)`
+
+	// ブログポストテーブルからデータを取得するSQL文
+	selectPostTable = `SELECT * FROM posts`
+
+	// ブログポストテーブルのデータを更新するSQL文
+	updatePostTable = `UPDATE posts SET title = ?, body = ?, author = ?, created_at = ? WHERE id = ?`
+
+	// ブログポストテーブルのデータを削除するSQL文
+	deletePostTable = `DELETE FROM posts WHERE id = ?`
+
+	// ブログポストテーブルのデータを全削除するSQL文
+	deleteAllPostTable = `DELETE FROM posts`
+
+)
+
 
 func main() {
 	http.HandleFunc("/", IndexHandler)
